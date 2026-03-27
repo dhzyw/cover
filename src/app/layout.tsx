1815c4e85c73d4ed4c5501a3a2268a294f524a24
大海资源网 <59426371+dhzyw@users.noreply.github.com>
2026-03-27 17:26:37 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,9 @@ import './assets/css/patterns.css'
 import { CoverProvider } from './components/coverContext'
 
 export const metadata: Metadata = {
-  title: 'ThisCover 封面生成器',
+  title: '大海封面-漂亮的封面生成器-大海资源网',
   description: '免费、漂亮的封面生成器',
-  keywords: ['ThisCover', '封面生成器', 'Cover Generator', '免费设计', '公众号封面', '文章配图', '设计工具', '在线设计', 'weizwz', '唯知笔记', '唯知为之'],
+  keywords: ['ThisCover', '封面生成器', 'Cover Generator', '免费设计', '公众号封面', '文章配图', '设计工具', '在线设计', 'dhzyw', '大海资源网', '大海资源'],
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon.ico',
